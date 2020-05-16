1cfdb241314e354eec20204d742e32f5dbde0d62
A.M. Official Account <44955993+AMOfficalGithub@users.noreply.github.com>
2020-05-16 12:22:03 -0400

    Add files via upload

---
+++ Matrixes.py
@@ -0,0 +1,27 @@
+#Implementing Matix Operations with Addition and Subtraction
+X= [[1,2,3], [4,5,6],[7,8,9]]
+Y=[[10,11,12],[13,14,15],[16,17,18]]
+
+#Initalizing a 3 x3 zero matris as a placeholder
+result=[[0,0,0]],[0,0,0],[0,0,0]
+# iterate through rows
+for i in range(len(X)):
+# iterate through columns
+    for j in range(len(X[0])):
+        result[i][j] = X[i][j] + Y[i][j]
+print(result)
+#Subtraction using two matrices
+
+X = [[10,11,12],[13,14,15],[16,17,18]]
+Y = [[1,2,3],[4,5,6],[7,8,9]]
+# Initialize a result placeholder
+result = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+# iterate through columns
+    for j in range(len(X[0])):
+        result[i][j] = X[i][j] - Y[i][j]
+print(result)
---
+++ current_time.py
@@ -0,0 +1,5 @@
+"""
+this script prints the current system time.
+"""
+import datetime
+print(datetime.datetime.today())
